9f9668306caf3b7853adc3341cf9c929a605a1b0
alex_nemynov <alexnemynov@yandex.ru>
2024-04-20 19:33:28 +0300

    first commit

---
+++ keyboards/bookmarks_kb.py
@@ -0,0 +1,31 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from lexicon import lexicon_ru
+
+
+# Функция для формирования инлайн-клавиатуры на лету
+def create_inline_kb(width: int,
+                     *args: str,
+                     **kwargs: str) -> InlineKeyboardMarkup:
+    # Инициализируем билдер
+    kb_builder = InlineKeyboardBuilder()
+    # Инициализируем список для кнопок
+    buttons: list[InlineKeyboardButton] = []
+
+    # Заполняем список кнопками из аргументов args и kwargs
+    if args:
+        for button in args:
+            buttons.append(InlineKeyboardButton(
+                text=lexicon_ru[button] if button in lexicon_ru else button,
+                callback_data=button))
+    if kwargs:
+        for button, text in kwargs.items():
+            buttons.append(InlineKeyboardButton(
+                text=text,
+                callback_data=button))
+
+    # Распаковываем список с кнопками в билдер методом row c параметром width
+    kb_builder.row(*buttons, width=width)
+
+    # Возвращаем объект инлайн-клавиатуры
+    return kb_builder.as_markup()
